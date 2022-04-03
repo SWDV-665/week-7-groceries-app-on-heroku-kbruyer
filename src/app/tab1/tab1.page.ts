8d707e83968105fe2759fe2a0ec94c3a91d96830
kbruyer <38817591+kbruyer@users.noreply.github.com>
2022-04-03 12:29:35 -0700

    Update

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,91 +1,51 @@
-import { Component } from '@angular/core';
-import { ToastController } from '@ionic/angular';
-import { AlertController } from '@ionic/angular';
-
+import { Component } from "@angular/core";
+import { ToastController } from "@ionic/angular";
+import { AlertController } from "@ionic/angular";
+import { GroceriesServiceService } from "../groceries-service.service";
+import { InputDialogServiceService } from "../input-dialog-service.service";
 @Component({
-  selector: 'app-tab1',
-  templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss'],
+  selector: "app-tab1",
+  templateUrl: "tab1.page.html",
+  styleUrls: ["tab1.page.scss"],
 })
 export class Tab1Page {
-  title = 'Grocery List';
-
-  items = [
-    {
-      name: 'Milk',
-      quantity: 2,
-    },
-    {
-      name: 'Bread',
-      quantity: 1,
-    },
-    {
-      name: 'Banana',
-      quantity: 3,
-    },
-    {
-      name: 'Sugar',
-      quantity: 1,
-    },
-  ];
+  title = "Grocery List";
 
   constructor(
     public toastController: ToastController,
-    public alertController: AlertController
+    public alertController: AlertController,
+    public dataService: GroceriesServiceService,
+    public InputDialogService: InputDialogServiceService
   ) {}
 
-  async removeItem(item, index) {
-    console.log('Removing Item - ', item, index);
+  loadItem() {
+    return this.dataService.getItems();
+  }
+
+  async editItem(item, index) {
+    console.log("Editing Item - ", item, index);
     const toast = await this.toastController.create({
-      message: 'Removing Item - ' + index + '...',
+      message: "Editing Item - " + index + "...",
       duration: 2000,
     });
     toast.present();
 
-    this.items.splice(index, 1);
+    this.InputDialogService.showPrompt(item, index);
+  }
+
+  async removeItem(item, index) {
+    console.log("Removing Item - ", item, index);
+    const toast = await this.toastController.create({
+      message: "Removing Item - " + index + "...",
+      duration: 2000,
+    });
+    toast.present();
+
+    this.dataService.removeItem(index);
   }
 
   addItem() {
-    console.log('Adding item');
-    this.showAddItemPrompt();
-  }
-
-  async showAddItemPrompt() {
-    const alert = await this.alertController.create({
-      cssClass: 'my-custom-class',
-      header: 'Add Item',
-      message: 'Please enter item...',
-      inputs: [
-        {
-          name: 'name',
-          type: 'text',
-          placeholder: 'Name',
-        },
-        {
-          name: 'quantity',
-          type: 'text',
-          placeholder: 'Quantity',
-        },
-      ],
-      buttons: [
-        {
-          text: 'Cancel',
-          role: 'cancel',
-          cssClass: 'secondary',
-          handler: item => {
-            console.log('Confirm Cancel');
-          },
-        },
-        {
-          text: 'Save',
-          handler: item => {
-            console.log('Confirm Save', item);
-            this.items.push(item);
-          },
-        },
-      ],
-    });
-
-    await alert.present();
+    console.log("Adding item");
+    this.InputDialogService.showPrompt();
   }
 }
